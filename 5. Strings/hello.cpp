30f4a48916f3be7228cd3aef6c9111798b06bc1c
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2025-03-27 20:14:23 +0530

    Delete 5. Strings directory

--- 5. Strings/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> // Include input-output stream library
-
-int main() {
-    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
-    return 0; // Indicate successful program termination
-}
