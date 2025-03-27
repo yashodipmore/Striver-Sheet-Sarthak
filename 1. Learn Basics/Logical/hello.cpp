49b9934211f26361bdffc351f31e3742773d1164
Jaykumar Girase <jaykumargirase56@gmail.com>
2025-03-27 17:25:41 +0530

    Your commit message

---
+++ 1. Learn Basics/Logical/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
+    return 0; // Indicate successful program termination
+}
